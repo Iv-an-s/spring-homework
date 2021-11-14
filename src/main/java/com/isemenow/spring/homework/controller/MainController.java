45eec70b71efaf00f81dd9df6bfd118de92e714c
Ivan <ivan_semenov@bk.ru>
2021-11-14 21:52:37 +0300

    main task completed

--- src/main/java/com/isemenow/spring/homework/controller/MainController.java
+++ src/main/java/com/isemenow/spring/homework/controller/MainController.java
@@ -1,9 +1,22 @@
 package com.isemenow.spring.homework.controller;
 
+import com.isemenow.spring.homework.repository.ProductRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    private ProductRepository productRepository;
 
+    public MainController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/products")
+    public String showProductsPage(Model model){
+        model.addAttribute("products", productRepository.getAllProductsList());
+        return "products_page";
+    }
 
 }
